962f824d4f22e65f67ebb550f5f217f9b3e44c58
frankie <frankie@bengder.cc>
2016-10-10 10:39:02 +0800

    create IndexController and update routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,11 @@
 <?php
 
 Route::group(['middleware' => 'admin'], function () {
-
+    Route::get('/', 'Admin\IndexController@index');
+    Route::get('index', 'Admin\IndexController@index');
 });
+
+// 认证路由...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
